a193ebd11ed66356f951cdcb266d5a748f22d5f6
luigi <luigi.gil@gmail.com>
2025-02-24 18:57:52 -0300

    remove achordion for now

--- ZlK7r/keymap.c
+++ ZlK7r/keymap.c
@@ -2,7 +2,7 @@
 #include "version.h"
 #define MOON_LED_LEVEL LED_LEVEL
 #define ML_SAFE_RANGE SAFE_RANGE
-#include "features/achordion.h"
+/*#include "features/achordion.h"*/
 
 enum custom_keycodes {
   RGB_SLD = ML_SAFE_RANGE,
@@ -16,8 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
     KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_TRANSPARENT, 
     KC_TRANSPARENT, KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_TRANSPARENT, 
-    KC_TRANSPARENT, KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_TRANSPARENT, 
-    KC_TRANSPARENT, KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_TRANSPARENT, KC_TRANSPARENT, KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_TRANSPARENT, 
                                                     MT(MOD_LCTL, KC_SPACE),MO(2),                                          MO(1),          KC_ENTER
   ),
   [1] = LAYOUT_voyager(
@@ -43,12 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 };
 
-
-
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (!process_achordion(keycode, record)) { return false; }
-
   switch (keycode) {
     case ST_MACRO_0:
     if (record->event.pressed) {
@@ -71,9 +65,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 
-void housekeeping_task_user(void) {
-  achordion_task();
-}
+/*void housekeeping_task_user(void) {*/
+/*  achordion_task();*/
+/*}*/
 
 /*uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t* record) {*/
 /*  // If you quickly hold a tap-hold key after tapping it, the tap action is*/
